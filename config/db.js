390cfecffacbdf5e93f38fb211a1185e60c0ba6e
warakornkn98 <warakornkn98@gmail.com>
2024-07-27 23:57:48 +0700

    tryconnectdb

--- config/db.js
+++ config/db.js
@@ -1,8 +1,20 @@
-var mysql = require('mysql');
+const mysql = require('mysql2');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "test"
-});
+const connectDB = async()=>{
+    try{
+        await mysql.createConnection({
+          host: "localhost",
+          user: "root",
+          password: "root",
+          database: "test"
+        });
+        
+        console.log('DB Connected')
+
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+module.exports = connectDB
